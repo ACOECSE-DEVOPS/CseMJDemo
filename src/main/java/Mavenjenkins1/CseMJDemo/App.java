b2d5909cb610d0cfad9fb4085bed33725cd12eb1
Durga prasad <Durga prasad@LAPTOP-LN9V9DGH>
2022-11-11 11:06:48 +0530

    This is first Jenkins Demo

---
+++ src/main/java/Mavenjenkins1/CseMJDemo/App.java
@@ -0,0 +1,13 @@
+package Mavenjenkins1.CseMJDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Jenkins World" );
+    }
+}
